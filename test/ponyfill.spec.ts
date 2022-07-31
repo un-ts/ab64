f7fec298b8dd4cfe39c121b41b951b76eb0fff63
JounQin <admin@1stg.me>
2022-07-31 12:28:44 +0800

    test: skip unavailable test case on Node 14

--- test/ponyfill.spec.ts
+++ test/ponyfill.spec.ts
@@ -1,8 +1,13 @@
 import { atob, btoa } from 'ab64/ponyfill'
 
-test('it should work same as the builtin', () => {
-  // eslint-disable-next-line sonar/deprecation -- it's fine on browser
-  expect(atob('SGVsbG8gV29ybGQh')).toBe(globalThis.atob('SGVsbG8gV29ybGQh'))
-  // eslint-disable-next-line sonar/deprecation -- it's fine on browser
-  expect(btoa('Hello World!')).toBe(globalThis.btoa('Hello World!'))
-})
+const LEGACY = 14
+
+test.runIf(process.env.TEST_ENV !== 'node' || +process.versions.node > LEGACY)(
+  'it should work same as the builtin',
+  () => {
+    // eslint-disable-next-line sonar/deprecation -- it's fine on browser
+    expect(atob('SGVsbG8gV29ybGQh')).toBe(globalThis.atob('SGVsbG8gV29ybGQh'))
+    // eslint-disable-next-line sonar/deprecation -- it's fine on browser
+    expect(btoa('Hello World!')).toBe(globalThis.btoa('Hello World!'))
+  },
+)
