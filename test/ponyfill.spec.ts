ac738c372bb870c758e0c21c8638844c0977361b
JounQin <admin@1stg.me>
2025-03-27 23:04:00 +0800

    test: detect `globalThis` instead

--- test/ponyfill.spec.ts
+++ test/ponyfill.spec.ts
@@ -1,11 +1,9 @@
 import { atob, btoa } from 'ab64/ponyfill'
 
-const MIN = 16
-
-test.skipIf(+process.versions.node < MIN)(
-  'it should work same as the builtin',
-  () => {
-    expect(atob('SGVsbG8gV29ybGQh')).toBe(globalThis.atob('SGVsbG8gV29ybGQh'))
-    expect(btoa('Hello World!')).toBe(globalThis.btoa('Hello World!'))
-  },
-)
+test.runIf(
+  typeof globalThis.atob === 'function' &&
+    typeof globalThis.btoa === 'function',
+)('it should work same as the builtin', () => {
+  expect(atob('SGVsbG8gV29ybGQh')).toBe(globalThis.atob('SGVsbG8gV29ybGQh'))
+  expect(btoa('Hello World!')).toBe(globalThis.btoa('Hello World!'))
+})
